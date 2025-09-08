b0a722613f8bb4debb80580ad62ffa55f0ef6be4
Carlos <carlos.martin-sanchez@mongodb.com>
2025-09-08 22:54:23 +0200

    re-generate docs

--- docs/assets/Authors-9hCc-Qxg.js
+++ docs/assets/Authors-9hCc-Qxg.js
@@ -1,8 +1,2 @@
-import { j as r } from "./index-DxODvHK4.js"
-function n({ authors: a }) {
-  return r.jsx(r.Fragment, {
-    children: a.map((o) => o.name).join(", ") || "No authors available",
-  })
-}
-export { n as A }
+import{j as r}from"./index-DxODvHK4.js";function n({authors:a}){return r.jsx(r.Fragment,{children:a.map(o=>o.name).join(", ")||"No authors available"})}export{n as A};
 //# sourceMappingURL=Authors-9hCc-Qxg.js.map
--- docs/assets/books-D3-CboFH.js
+++ docs/assets/books-D3-CboFH.js
@@ -1,7 +1,2 @@
-import { j as r } from "./index-DxODvHK4.js"
-const s = ({ error: e }) =>
-  r.jsxs("mark", {
-    children: ["There was an error: ", r.jsx("code", { children: e.message })],
-  })
-export { s as errorComponent }
+import{j as r}from"./index-DxODvHK4.js";const s=({error:e})=>r.jsxs("mark",{children:["There was an error: ",r.jsx("code",{children:e.message})]});export{s as errorComponent};
 //# sourceMappingURL=books-D3-CboFH.js.map
